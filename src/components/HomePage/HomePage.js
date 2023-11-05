1332cd1744a3dd637740ce461e28852b74f3822b
Maksym Podhornyi <forjant1@gmail.com>
2023-11-05 16:20:09 +0100

    added check on login

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,18 +3,26 @@ import HomePageStyle from "./HomePage.module.css";
 import HeaderImage from "../images/header_img.png";
 import Vector from "../images/vector.png";
 import UserAvatar from "../images/user_avatar.png";
-import { fixElementHeight, fixElementWidth } from "../Utils";
+import { fixElementHeight, fixElementWidth, ifUserLoggedIn } from "../Utils";
 import "../GlobalStyles.css";
 
 const HomePage = () => {
 
     const elementRef = useRef(null);
-    const elementRef2 = useRef(null);
+    const logInRef = useRef(null);
+    const loggedIn = useRef(null);
 
     useEffect(() => {
         if (elementRef.current) {
             fixElementHeight(elementRef.current);
-            fixElementWidth(elementRef2.current);
+            fixElementWidth(logInRef.current);
+            fixElementWidth(loggedIn.current);
+        }
+
+        if (ifUserLoggedIn()) {
+            loggedIn.current.style.display = 'flex';
+        } else {
+            logInRef.current.style.display = 'flex';
         }
     }, []);
 
@@ -33,17 +41,17 @@ const HomePage = () => {
                 <b className="women">Women</b>
                 <b className="kids">Kids</b>
 
-                {/* <div className={HomePageStyle['log-in']} id="logInContainer" ref={elementRef2}>
+                <div className={HomePageStyle['log-in-container']} id="log-in-container" ref={logInRef}>
                     <b className={HomePageStyle['log-in-text']}>Log In</b>
                     <img className={HomePageStyle['log-in-icon']} alt="" src={Vector} />
-                </div> */}
+                </div>
 
-                <div className="logged-in-container">
+                <div className={HomePageStyle['logged-in-container']} ref={loggedIn}>
                     <b className={HomePageStyle['logged-in-as']}>Logged in as John</b>
                     <img
-                    className={HomePageStyle['icon-user-avatar-icon']}
-                    alt=""
-                    src={UserAvatar}
+                        className={HomePageStyle['icon-user-avatar-icon']}
+                        alt=""
+                        src={UserAvatar}
                     />
                 </div>
             </div>
--- src/components/Utils.js
+++ src/components/Utils.js
@@ -15,4 +15,39 @@ export const fixElementWidth = (element) => {
 
     }
 }
+
+export const ifUserLoggedIn = async () => {
+    const cookies = document.cookie.split(';');
+    const userId = cookies.find(cookie => cookie.includes('user_id'));
+    if (userId) {
+
+        const data = {
+            user_id: userId.split('=')[1]
+        };
+
+        try {
+            const response = await fetch("http://localhost:8080/api/v1.0/verify", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (response.ok) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            return false;
+        }
+
+    }
+    return false;
+}
+
+
+    
   
