4523c71696af0b1e5d37d7719199d9e40a7ee23d
Maksym Podhornyi <forjant1@gmail.com>
2023-12-16 17:14:28 +0100

    added styles

--- src/components/Chats/ChatsPage.js
+++ src/components/Chats/ChatsPage.js
@@ -22,6 +22,8 @@ const ChatsPage = () => {
 
     const [chats, setChats] = useState([]);
 
+    const textsttst = `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.`
+
     const addChat = (username, item_name, chat_id, item_id) => {
         return (
             <div className={ChatsPageStyles['one-chat-container']} key = {chat_id} onClick={openChat} id = {chat_id}
@@ -168,31 +170,13 @@ const ChatsPage = () => {
                         <span className={ChatsPageStyles["item-id-header"]} ref = {chatHeader}></span>
                     </div>
                     <div className={ChatsPageStyles['each-chat-messages']}>
-                        <div className={ChatsPageStyles['each-chat-message-container']}>
+                        <div className={ChatsPageStyles['each-chat-message-container-mine']}>
                             <span className={ChatsPageStyles['each-chat-message']}>Hello</span>
                         </div>
-                        <div className={ChatsPageStyles['each-chat-message-container1']}>
+                        <div className={ChatsPageStyles['each-chat-message-container-yours']}>
                             <span className={ChatsPageStyles['each-chat-message']}>Hello</span>
                         </div>
-                        <div className={ChatsPageStyles['each-chat-message-container']}>
-                            <span className={ChatsPageStyles['each-chat-message']}>Hello</span>
-                        </div>
-                        <div className={ChatsPageStyles['each-chat-message-container']}>
-                            <span className={ChatsPageStyles['each-chat-message']}>Hello</span>
-                        </div>
-                        <div className={ChatsPageStyles['each-chat-message-container']}>
-                            <span className={ChatsPageStyles['each-chat-message']}>Hello</span>
-                        </div>
-                        <div className={ChatsPageStyles['each-chat-message-container']}>
-                            <span className={ChatsPageStyles['each-chat-message']}>Hello</span>
-                        </div>
-                        <div className={ChatsPageStyles['each-chat-message-container']}>
-                            <span className={ChatsPageStyles['each-chat-message']}>Hello</span>
-                        </div>
-                        <div className={ChatsPageStyles['each-chat-message-container']}>
-                            <span className={ChatsPageStyles['each-chat-message']}>Hello</span>
-                        </div>
-                        <div className={ChatsPageStyles['each-chat-message-container']}>
+                        <div className={ChatsPageStyles['each-chat-message-container-yours']}>
                             <span className={ChatsPageStyles['each-chat-message']}>Hello</span>
                         </div>
                     </div>
