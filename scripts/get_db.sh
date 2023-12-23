6a2ca4569cd31c2e949c54876d08387d4eb4c8f3
Maksym Podhornyi <forjant1@gmail.com>
2023-12-23 05:38:17 +0100

    debug

--- scripts/get_db.sh
+++ scripts/get_db.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-docker cp $(docker ps | grep 'backend:latest' | awk '{print $1}'):app/src/database.db ../backend/src/database.db
+CONTAINER_ID=$(docker ps | grep 'backend:latest' | awk '{print $1}')
+if [ ! -z "$CONTAINER_ID" ]; then
+  docker cp "${CONTAINER_ID}":/app/src/database.db ../backend/src/database.db
+else
+  echo "No running backend:latest container found."
+fi
