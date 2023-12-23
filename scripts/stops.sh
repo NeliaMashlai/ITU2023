ca55450230d00ca31ed666124ad44687aea61f34
Maksym Podhornyi <forjant1@gmail.com>
2023-12-23 05:36:55 +0100

    debug

--- scripts/stops.sh
+++ scripts/stops.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker stop $(docker ps | grep "frontend:latest" | awk '{print $1}')
-docker stop $(docker ps | grep "backend:latest" | awk '{print $1}')
+docker ps | grep "frontend:latest" | awk '{print $1}' | xargs -r docker stop
+docker ps | grep "backend:latest" | awk '{print $1}' | xargs -r docker stop
