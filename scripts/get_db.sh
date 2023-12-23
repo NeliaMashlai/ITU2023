bdd889c27e8d20de279c99c1c1242459960f9935
Maksym Podhornyi <forjant1@gmail.com>
2023-12-23 05:39:45 +0100

    debug

--- scripts/get_db.sh
+++ scripts/get_db.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 CONTAINER_ID=$(docker ps | grep 'backend:latest' | awk '{print $1}')
+echo $(docker ps)
 if [ ! -z "$CONTAINER_ID" ]; then
   docker cp "${CONTAINER_ID}":/app/src/database.db ../backend/src/database.db
 else
