a82bc6df81fcda26d48ff77da402b13d9eccb1cf
Maksym Podhornyi <forjant1@gmail.com>
2023-12-23 05:42:48 +0100

    deb

--- scripts/stops.sh
+++ scripts/stops.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker ps | grep "frontend:latest" | awk '{print $1}' | xargs -r docker stop
-docker ps | grep "backend:latest" | awk '{print $1}' | xargs -r docker stop
+docker ps | grep "python run.py" | awk '{print $1}' | xargs -r docker stop
